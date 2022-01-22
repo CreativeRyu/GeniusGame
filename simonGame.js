521413f94b0557f135a01c0d13f74737a4fbf351
CreativeRyu <Creative.Ryu@web.de>
2022-01-22 18:17:41 +0100

    implemented main game logic

--- simonGame.js
+++ simonGame.js
@@ -1,18 +1,85 @@
 
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-nextSequence();
+var userClickedPattern = [];
+var level = 0;
+
+listenToButtonClicks();
+
+function startSequence() {
+    animatePress("play");
+    nextSequence();
+}
 
 function nextSequence() {
+    // Spielerpattern muss vor jeder neuen Runde geleert werden
+    userClickedPattern = [];
+
     // Get Button
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    level++
+    $("#level-title").text("Level " + level);
 
-    // Animate Button
+    // Animate Colour Buttons Button
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     // Play Sounds of Buttons
-    var sound = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+// Animation for Play Button
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+}
+
+function gameOver() {
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+        $("body").removeClass("game-over")
+    }, 200);
+
+    $("#level-title").text("GAME OVER, Press Start Button to restart.");
+    gamePattern = [];
+    level = 0;
+}
+
+// Button Handler which generates UserPattern
+// User clicks Buttons
+function listenToButtonClicks() {
+    $(".btn").click(function () {
+        var userChosenColour = this.id;
+        animatePress(userChosenColour);
+        userClickedPattern.push(userChosenColour);
+        playSound(userChosenColour);
+        console.log(userClickedPattern);
+        checkInput(userClickedPattern.length - 1);
+    });
+}
+
+function playSound(name) {
+    var sound = new Audio("sounds/" + name + ".mp3");
     sound.play();
+}
+
+function checkInput(currentLevel) {
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        gameOver();
+    }
 }
