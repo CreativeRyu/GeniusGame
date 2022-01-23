b78975acdd27501ff210cae04d5ad84a2da60e63
CreativeRyu <Creative.Ryu@web.de>
2022-01-23 13:10:40 +0100

    add more usablity

--- geniusGame.js
+++ geniusGame.js
@@ -8,34 +8,40 @@ listenToButtonClicks();
 
 function startSequence() {
     animatePress("play");
-    nextSequence();
+    setTimeout(() => {
+        nextSequence();
+    }, 1000);
 }
 
-function nextSequence() {
+async function nextSequence() {
     // Spielerpattern muss vor jeder neuen Runde geleert werden
     userClickedPattern = [];
 
     // Get Button
     var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+    var randomChosenColor = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColor);
     level++
     $("#level-title").text("Level " + level);
 
-    // Animate Colour Buttons Button
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    for (index = 0; index < gamePattern.length; index++) {
+        var gameIndex = gamePattern[index];
 
-    // Play Sounds of Buttons
-    playSound(randomChosenColour);
+        animatePress(gameIndex);
+        playSound(gameIndex);
+        await sleep(800);
+    }
 }
 
 // Animation for Play Button
 function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-
+    setTimeout(() => {
+        $("#" + currentColour).addClass(currentColour + "-pressed");
+    }, 300);
+    
     setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed")
-    }, 100);
+        $("#" + currentColour).removeClass(currentColour + "-pressed")
+    }, 500);
 }
 
 function gameOver() {
@@ -80,4 +86,8 @@ function checkInput(currentLevel) {
         playSound("wrong");
         gameOver();
     }
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
